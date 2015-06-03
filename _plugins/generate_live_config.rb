2e1c2a9cf541273454b1e48f87350b644869f75e
Daniel Groves <hello@danielgroves.net>
2015-06-03 19:41:13 +0100

    Load CSS and JS from corrects URLs
    Don't be a muppet next time.

--- _plugins/generate_live_config.rb
+++ _plugins/generate_live_config.rb
@@ -3,6 +3,8 @@ module Jekyll
     def generate(site)
       if (ENV['JEKYLL_ENV'] == 'production')
         site.config['static_path'] = 'https://d1238u3jnb0njy.cloudfront.net'
+        site.config['styles_path'] = 'https://d1238u3jnb0njy.cloudfront.net'
+        site.config['scripts_path'] = 'https://d1238u3jnb0njy.cloudfront.net'
       end
     end
   end
